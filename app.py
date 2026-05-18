0c579f2c2cb02b97eceea229afdd45e68050b98f
GDS23 <gedson23santos@hotmail.com>
2026-05-18 14:05:51 -0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import time
+
+def inicio():
+    print("=" * 35)
+    print("  PROJETO CRIADO NO ACODE ")
+    print("  Enviado pelo celular ")
+    print("=" * 35)
+    
+    print("\nIniciando sistema...")
+    time.sleep(1)
+    print("Carregando módulos...")
+    time.sleep(1)
+    print("Tudo pronto! Código funcionando.\n")
+
+if __name__ == "__main__":
+    inicio()
